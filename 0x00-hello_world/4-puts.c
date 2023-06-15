2944164f5fd9ff93d017199fab9492b837885e81
Anas Ma <Anass.mamlouk@gmail.com>
2023-06-16 01:43:53 +0200

    2uuup

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main (void)
+/**
+  * main - main entry point
+  *
+  * Description: using puts fonction
+  *
+  * Return: always 0 (Success)
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
